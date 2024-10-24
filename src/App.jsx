0c5d6be143ee78a8e799a8c57258be96e4e26f23
jbjoybiswasjb <jbjoybiswasjb@gmail.com>
2024-10-24 19:22:25 +0600

    use useState for state bottles data.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { useState } from 'react'
 import './App.css'
 import Bottles from './components/Bottles/Bottles'
 
 function App() {
+
+  // State Bottles data.
+  const [bottles, setBottles] = useState([]);
 
   return (
     <>
