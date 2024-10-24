8ade2f16c06c4d111bd8ca4dea4b3a635db5ccab
jbjoybiswasjb <jbjoybiswasjb@gmail.com>
2024-10-24 19:29:08 +0600

    Load bottles data using useEffect.

--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -1,13 +1,22 @@
+import { useEffect } from 'react';
 import './Bottles.css';
+import { useState } from 'react';
 
 const Bottles = () => {
 
     // State Bottles data.
     const [bottles, setBottles] = useState([]);
 
+    // Load Bottles data.
+    useEffect(() => {
+        fetch('bottles.json')
+            .then(response => response.json())
+            .then(data => setBottles(data))
+    }, []);
+
     return (
         <div className="bottles">
-            <h2>Bottles: </h2>
+            <h2>Bottles: {bottles.length}</h2>
         </div>
     );
 };
