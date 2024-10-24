ec330330d1109b4dc49a745c3e30308401d9e724
jbjoybiswasjb <jbjoybiswasjb@gmail.com>
2024-10-24 19:11:57 +0600

    Added Bottles component in App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Bottles from './components/Bottles/Bottles'
 
 function App() {
 
   return (
     <>
       <h1>Bottles shop</h1>
+
+      <Bottles></Bottles>
     </>
   )
 }
