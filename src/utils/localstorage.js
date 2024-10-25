55a17a78b2d14fd490e4eaef6e9aadf5c4d57584
jbjoybiswasjb <jbjoybiswasjb@gmail.com>
2024-10-25 07:39:17 +0600

    conver the storedCartArray to string.

--- src/utils/localstorage.js
+++ src/utils/localstorage.js
@@ -8,10 +8,19 @@ const getStoredCart = () => {
 }
 
 
+const saveCartToLS = storedCartArray => {
+    const newCart = JSON.stringify(storedCartArray);
+}
+
+
 // Add new item in existing cart.
 const addCartToLS = id => {
 
     // Add item in storedCartArray.
     const storedCartArray = getStoredCart();
     storedCartArray.push(id);
+
+    // Then convert the storedCartArray from array to string.
+    saveCartToLS(storedCartArray);
+    
 }
