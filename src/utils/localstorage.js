b72a859906f45585baf4ba6847cb94457b2d5eb0
jbjoybiswasjb <jbjoybiswasjb@gmail.com>
2024-10-25 07:23:33 +0600

    If there is any item of storedCart in local storage, convert it string to array then return.

--- src/utils/localstorage.js
+++ src/utils/localstorage.js
@@ -1,5 +1,7 @@
 // Get stored cart items from LS.
 const getStoredCart = () => {
     const storedCart = localStorage.getItem('cart');
-    
+    if (storedCart) {
+        return JSON.parse(storedCart);
+    }
 }
